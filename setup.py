8136bdcdf0e86b0d56b4e491abbb91114bcf3160
s0i37 <s0i37@work>
2024-02-04 00:20:07 +0500

    pip changes

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+  name = 'tracebuster',
+  version = '1.0.2',
+  description = 'fast tracing networks tool and finding live subnets',
+  author = 's0i37',
+  author_email = 's0i37@yahoo.com',
+  url = 'https://github.com/s0i37/tracebuster',
+  keywords = ['traceroute', 'reconnaissance'],
+  classifiers = [],
+  scripts=['bin/tracebuster'],
+  install_requires=[
+    'scapy',
+    'netaddr',
+    'pydot'
+  ]
+)
